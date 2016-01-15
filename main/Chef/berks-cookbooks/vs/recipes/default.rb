30a053775fe93b621e985ab6dd4f4e4562ee15e5
Timothy Bassett <tim@the.bassetts.name>
2016-01-15 02:10:44 -0500

    spike of finding the correct volume working

---
+++ main/Chef/berks-cookbooks/vs/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: vs
+# Recipe:: default
+#
+# Copyright (C) 2016 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'powershell'
+puts 'vs............'
