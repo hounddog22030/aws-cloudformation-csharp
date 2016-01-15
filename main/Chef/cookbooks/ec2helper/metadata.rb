ca2e3e3789cf1b59964e713db3ef3da34b5b0da4
Timothy Bassett <tim@the.bassetts.name>
2016-01-15 12:17:02 -0500

    new style

---
+++ main/Chef/cookbooks/ec2helper/metadata.rb
@@ -0,0 +1,9 @@
+name             'ec2helper'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures ec2helper'
+long_description 'Installs/Configures ec2helper'
+
+version          '0.1.0'
+
