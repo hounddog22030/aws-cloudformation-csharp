2cfffdd5b7a93dfdbfcc1ddcbbbe68a91f39f029
Timothy Bassett <tim@the.bassetts.name>
2016-01-18 11:36:34 -0500

    new tfs

---
+++ main/Chef/cookbooks/tfs/metadata.rb
@@ -0,0 +1,9 @@
+name             'tfs'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures tfs'
+long_description 'Installs/Configures tfs'
+
+version          '0.1.0'
+
