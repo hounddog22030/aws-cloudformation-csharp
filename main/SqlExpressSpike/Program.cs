1a549a3086dfab907f3ca1649d4a7a6ef7262493
Timothy Bassett <tim@the.bassetts.name>
2016-02-03 19:57:55 -0500

    sqlexpress spike can connect to rds

---
+++ main/SqlExpressSpike/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlExpressSpike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Connection String:");
+            var connectionString = Console.ReadLine();
+            using (var c = new SqlConnection(connectionString))
+            {
+                c.Open();
+                var command = c.CreateCommand();
+                command.CommandText = "SELECT 0";
+                var result = command.ExecuteScalar();
+                Console.Write("Result:");
+                Console.WriteLine(result.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
