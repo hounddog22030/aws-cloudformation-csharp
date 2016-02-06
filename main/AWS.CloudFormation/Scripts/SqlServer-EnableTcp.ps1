1c1485726c22925aa516ef8139fb0cdd6331a885
Timothy Bassett <tim@the.bassetts.name>
2016-02-06 06:38:50 -0500

    ?

---
+++ main/AWS.CloudFormation/Scripts/SqlServer-EnableTcp.ps1
@@ -0,0 +1,19 @@
+﻿# Load the assemblies
+[reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
+[reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.SqlWmiManagement")
+
+$smo = 'Microsoft.SqlServer.Management.Smo.'
+$wmi = new-object ($smo + 'Wmi.ManagedComputer').
+
+# List the object properties, including the instance names.
+$Wmi
+
+# Enable the TCP protocol on the default instance.
+$uri = "ManagedComputer[@Name='" + (get-item env:\computername).Value + "']/ServerInstance[@Name='MSSQLSERVER']/ServerProtocol[@Name='Tcp']"
+$Tcp = $wmi.GetSmoObject($uri)
+$Tcp.IsEnabled = $true
+$Tcp.Alter()
+$Tcp
+
+NET STOP MSSQLSERVER
+NET START MSSQLSERVER
---
+++ main/AWS.CloudFormation/Scripts/add-network-to-sysadmin.ps1
@@ -0,0 +1,29 @@
+﻿Import-Module SQLPS -DisableNameChecking
+$Computer = $env:COMPUTERNAME
+$SqlServices = Get-Service -DisplayName 'SQL Server (*'
+Write-Host $SqlServices
+$InstanceNames = $SqlServices.DisplayName.Split('(')[1].Trim(')')
+Write-Host "InstanceNames:$InstanceNames"
+
+foreach ($Instance in $InstanceNames) {
+    Write-Host "Instance: $Instance"
+    if ($Instance -eq 'MSSQLSERVER') {
+            ## If a default instance only use the server name and don’t specify an instance
+            $Server = new-object ('Microsoft.SqlServer.Management.Smo.Server') $Computer
+    } else {
+            ## If a named instance use the server name and the instance name
+            $Server = new-object ('Microsoft.SqlServer.Management.Smo.Server') "$Computer`$Instance"
+     }
+    ## Output all Microsoft.SqlServer.Management.Smo.Login objects
+    ##$Server.Logins
+    
+    #$Server = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server -ArgumentList $Instance
+    Write-Host "Server:$Server"
+    $login = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Login($Server, "NT AUTHORITY\NETWORK SERVICE")
+    $login.LoginType = [Microsoft.SqlServer.Management.Smo.LoginType]::WindowsUser
+    $login.Create()
+    $login.AddToRole('sysadmin')
+    $login.AddToRole('serveradmin')
+    Write-Host "Login:$Login"
+    ##$Login.LoginType = "WindowsUser"
+}
