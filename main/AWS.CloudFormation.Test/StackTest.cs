da9801b6479aab0f237ec1e8eb41cd9b18a4a928
Timothy Bassett <tim@the.bassetts.name>
2016-02-11 00:10:20 -0500

    rho fullstack

--- main/AWS.CloudFormation.Test/StackTest.cs
+++ main/AWS.CloudFormation.Test/StackTest.cs
@@ -1418,7 +1418,7 @@ namespace AWS.CloudFormation.Test
 
             Create instances = Create.FullStack;
             instances = Create.Dc2 | Create.Sql4Tfs | Create.Workstation | Create.BackupServer | Create.Rdp1 | Create.Tfs;
-            //instances = Create.Dc2 | Create.Workstation | Create.BackupServer | Create.Rdp1 ;
+            instances = Create.FullStack;
 
 
             var template = GetTemplateFullStack("yadayadasoftware.com", "dev", version, instances);
