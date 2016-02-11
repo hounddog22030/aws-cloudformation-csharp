3013727b16c92e032aae1dafe794ef036d95e741
Timothy Bassett <tim@the.bassetts.name>
2016-02-10 21:36:06 -0500

    minimal stack test

--- main/AWS.CloudFormation.Test/StackTest.cs
+++ main/AWS.CloudFormation.Test/StackTest.cs
@@ -1389,7 +1389,7 @@ namespace AWS.CloudFormation.Test
             var appName = "dev";
 
             //Create instances = Create.Dc2 | Create.BackupServer | Create.Rdp1;
-            Create instances = Create.Dc2 | Create.BackupServer | Create.Rdp1 | Create.Workstation;
+            Create instances = 0;
             var templateToCreateStack = GetTemplateFullStack(topLevel, appName, version, instances);
             templateToCreateStack.StackName = $"{version}-{appName}-{topLevel}".Replace('.','-');
 
