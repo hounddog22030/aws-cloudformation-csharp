4f402312b82284c281521c492353e6a509eb335a
Timothy Bassett <tim@the.bassetts.name>
2016-02-13 12:18:44 -0500

    drop sql/tfs

--- main/AWS.CloudFormation.Test/StackTest.cs
+++ main/AWS.CloudFormation.Test/StackTest.cs
@@ -1433,7 +1433,7 @@ namespace AWS.CloudFormation.Test
             //instances = Create.Dc2 | Create.Sql4Tfs | Create.Workstation | Create.BackupServer | Create.Rdp1 | Create.Tfs;
             instances = (Create)0;
             //instances = Create.Dc2 | Create.Workstation | Create.BackupServer | Create.Rdp1;
-            instances = Create.Dc2 | Create.Sql4Tfs | Create.BackupServer | Create.Tfs | Create.Build;
+            instances = Create.Dc2  | Create.BackupServer | Create.Build | Create.Workstation;
             //            Create.Workstation;
 
 
