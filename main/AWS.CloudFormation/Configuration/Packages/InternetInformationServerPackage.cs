e65f6d34c53b3c9b8d2c283a4fe11a6754d38baa
Timothy Bassett <tim@the.bassetts.name>
2016-02-14 17:13:12 -0500

    added webdeploy to iis

--- main/AWS.CloudFormation/Configuration/Packages/InternetInformationServerPackage.cs
+++ main/AWS.CloudFormation/Configuration/Packages/InternetInformationServerPackage.cs
@@ -13,6 +13,7 @@ namespace AWS.CloudFormation.Configuration.Packages
         
         public InternetInformationServerPackage(string snapshotId, string bucketName, string cookbookName): base(snapshotId, bucketName, cookbookName)
         {
+            //https://technet.microsoft.com/en-us/library/dd569059.aspx
         }
 
         public override void AddToLaunchConfiguration(LaunchConfiguration configuration)
@@ -21,17 +22,21 @@ namespace AWS.CloudFormation.Configuration.Packages
 
             var secondConfigSetName = $"ConfigSet{this.ConfigName}HttpToHttps";
             var secondConfigName = $"Config{this.ConfigName}HttpToHttps";
-            var secondConfig =
-                configuration.Metadata.Init.ConfigSets.GetConfigSet(secondConfigSetName).GetConfig(secondConfigName);
+            var secondConfig = configuration.Metadata.Init.ConfigSets.GetConfigSet(secondConfigSetName).GetConfig(secondConfigName);
+            secondConfig.Files.GetFile("c:/inetpub/wwwroot/web.config").Source = "https://s3.amazonaws.com/gtbb/web.config";
+            secondConfig.Files.GetFile("c:/inetpub/wwwroot/healthcheck.htm").Source = "https://s3.amazonaws.com/gtbb/healthcheck.htm";
 
             var msiUri = new Uri("https://s3.amazonaws.com/gtbb/software/rewrite_amd64.msi");
-
             var fileName = System.IO.Path.GetFileNameWithoutExtension(msiUri.AbsolutePath).Replace(".", string.Empty).Replace("-", String.Empty);
             var msi = new CloudFormationDictionary();
             msi.Add(fileName, msiUri.AbsoluteUri);
             secondConfig.Packages.Add("msi", msi);
-            secondConfig.Files.GetFile("c:/inetpub/wwwroot/web.config").Source = "https://s3.amazonaws.com/gtbb/web.config";
-            secondConfig.Files.GetFile("c:/inetpub/wwwroot/healthcheck.htm").Source = "https://s3.amazonaws.com/gtbb/healthcheck.htm";
+
+            msiUri = new Uri($"https://s3.amazonaws.com/{BucketName}/software/WebDeploy_amd64_en-US.msi");
+            fileName = System.IO.Path.GetFileNameWithoutExtension(msiUri.AbsolutePath).Replace(".", string.Empty).Replace("-", String.Empty);
+            msi = new CloudFormationDictionary();
+            msi.Add(fileName, msiUri.AbsoluteUri);
+            secondConfig.Packages.Add("msi", msi);
         }
     }
 }
