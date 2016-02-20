662e89260b13a218e3f08bc573824b6a8e5b5836
Timothy Bassett <tim@the.bassetts.name>
2016-02-20 02:02:55 -0500

    clearn join?

--- main/AWS.CloudFormation/Resource/EC2/DHCPOptions.cs
+++ main/AWS.CloudFormation/Resource/EC2/DHCPOptions.cs
@@ -17,13 +17,14 @@ namespace AWS.CloudFormation.Resource.EC2
         public DhcpOptions(object domainName, Vpc vpc, FnJoin dnsServers, FnJoin netBiosNameServers) 
             : base(ResourceType.DhcpOptions)
         {
-            if (domainName != null)
-            {
-                this.DomainName = domainName;
-            }
-            this.Vpc = vpc;
-            this.AddDomainNameServer(dnsServers);
-            this.AddNetBiosNameServers(netBiosNameServers);
+            throw new NotImplementedException();
+            //if (domainName != null)
+            //{
+            //    this.DomainName = domainName;
+            //}
+            //this.Vpc = vpc;
+            //this.AddDomainNameServer(dnsServers);
+            //this.AddNetBiosNameServers(netBiosNameServers);
         }
 
         public DhcpOptions(Vpc vpc, SimpleAd simpleAd) : base(ResourceType.DhcpOptions)
@@ -95,47 +96,6 @@ namespace AWS.CloudFormation.Resource.EC2
             {
                 this.Properties.SetValue(value);
             }
-        }
-
-        //"NetbiosNameServers" : [
-        //    {
-        //        "Ref" : "AD1PrivateIp"
-        //    },
-        //    {
-        //        "Ref" : "AD2PrivateIp"
-        //    }
-        //],
-        //"NetbiosNodeType"    : "2",
-
-        public void AddDomainNameServer(object domainNameServer)
-        {
-            var temp = new List<object>();
-            IEnumerable<object> current = null;
-            if (this.Properties.ContainsKey("DomainNameServers"))
-            {
-                current = this.Properties["DomainNameServers"] as IEnumerable<object>;
-            }
-            if (current != null)
-            {
-                temp.AddRange(current);
-            }
-            temp.Add(domainNameServer);
-            this.Properties["DomainNameServers"] = temp;
-        }
-        private void AddNetBiosNameServers(object netBiosNameServer)
-        {
-            var temp = new List<object>();
-            IEnumerable<object> current = null;
-            if (this.Properties.ContainsKey("NetbiosNameServers"))
-            {
-                current = this.Properties["NetbiosNameServers"] as IEnumerable<object>;
-            }
-            if (current != null)
-            {
-                temp.AddRange(current);
-            }
-            temp.Add(netBiosNameServer);
-            this.Properties["NetbiosNameServers"] = temp;
         }
     }
 }
