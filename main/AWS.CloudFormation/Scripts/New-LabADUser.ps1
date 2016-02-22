8a23b415e633bac0e2ae02f454c779cf4f44159c
Timothy Bassett <tim@the.bassetts.name>
2016-02-21 20:11:00 -0500

    fixes to creating users

--- main/AWS.CloudFormation/Scripts/New-LabADUser.ps1
+++ main/AWS.CloudFormation/Scripts/New-LabADUser.ps1
@@ -1,38 +1,37 @@
-﻿begin {
-    $users = Import-Csv $PSScriptRoot\users.csv
-}
-
-process {
-	
-$Root = [ADSI]"LDAP://RootDSE"
-		$Root
-
-foreach ($user in $users) {
- 
-	    $firstname = $user.firstname
-	    $lastname = $user.lastname
-		
-#	    $upn = "$($firstname[0])$lastname@$UpnSuffix".ToLower()
-	    $name = "$firstname $lastname"
-	    $alias = "$($firstname[0])$lastname".ToLower()
-		
-		$Search = "LDAP://CN=Users," + $Root.rootDomainNamingContext
-		$Search
-		$Container =  [ADSI]($Search)
-		$Container
-		$usr = $Container.Create("user","cn=$alias")
-		$usr.Put("sAMAccountName",$alias)
-		$usr.CommitChanges()
-
-		$Search = "LDAP://CN=" + $alias + ",CN=Users," + $Root.rootDomainNamingContext
-		$Search
-		$objUser = [ADSI]$Search
-		$objUser
-		$user.password
-		$objUser.SetPassword($user.password)
-		$objUser.userAccountControl
-		$objUser.userAccountControl = 544
-		$objUser.userAccountControl
-		$objUser.CommitChanges()
-    }
+﻿begin {
+    $users = Import-Csv $PSScriptRoot\users.csv
+}
+
+process {
+	
+$Root = [ADSI]"LDAP://RootDSE"
+		$Root
+
+foreach ($user in $users) {
+ 
+	    $firstname = $user.firstname
+	    $lastname = $user.lastname        $password = $user.password                Write-Host "password=$password"        Write-Host "firstname=$firstname"
+		
+#	    $upn = "$($firstname[0])$lastname@$UpnSuffix".ToLower()
+	    $name = "b$firstname $lastname"
+	    $alias = "b$($firstname[0])$lastname".ToLower()        
+		
+		$Search = "LDAP://CN=Users," + $Root.rootDomainNamingContext
+		$Search
+		$Container =  [ADSI]($Search)
+		$Container
+		$usr = $Container.Create("user","cn=$alias")
+		$usr.Put("sAMAccountName",$alias)
+		$usr.CommitChanges()
+
+		$Search = "LDAP://CN=" + $alias + ",CN=Users," + $Root.rootDomainNamingContext
+		$Search
+		$objUser = [ADSI]$Search
+		$objUser        
+		$objUser.SetPassword($password)
+		$objUser.userAccountControl
+		$objUser.userAccountControl = 544
+		$objUser.userAccountControl
+		$objUser.CommitChanges()
+    }
 }
