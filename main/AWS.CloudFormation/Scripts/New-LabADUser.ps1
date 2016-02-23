3d74821bf087338041ad58d443b6c4e2241987a9
Timothy Bassett <tim@the.bassetts.name>
2016-02-23 18:32:12 -0500

    fix create users

--- main/AWS.CloudFormation/Scripts/New-LabADUser.ps1
+++ main/AWS.CloudFormation/Scripts/New-LabADUser.ps1
@@ -1,33 +1,33 @@
-﻿begin {
-    $users = Import-Csv $PSScriptRoot\users.csv
-}
-
-process {
-	$Root = [ADSI]"LDAP://RootDSE"
-	$Root
-
-	foreach ($user in $users) {
-			$firstname = $user.firstname
-			$lastname = $user.lastname
-			$principal = $users.principal
-			$name = "$firstname $lastname"
-			$Search = "LDAP://CN=Users," + $Root.rootDomainNamingContext
-			$Container =  [ADSI]($Search)
-			$Container
-			$usr = $Container.Create("user","cn=$name")
-			$usr.Put("sAMAccountName",$principal)
-			$usr.CommitChanges()
-
-			$Search = "LDAP://CN=" + $principal + ",CN=Users," + $Root.rootDomainNamingContext
-			$Search
-			$objUser = [ADSI]$Search
-			$objUser
-			$password = $users.password
-			Write-Host "password=$password"
-			$objUser.SetPassword('H3ll0!23$5!!')
-			$objUser.userAccountControl
-			$objUser.userAccountControl = 66048
-			$objUser.userAccountControl
-			$objUser.CommitChanges()
-		}
+﻿begin {
+    $users = Import-Csv $PSScriptRoot\users.csv
+}
+
+process {
+	$Root = [ADSI]"LDAP://RootDSE"
+	$Root
+
+	foreach ($user in $users) {
+			$firstname = $user.firstname
+			$lastname = $user.lastname
+			$principal = $user.principal
+			$name = "$firstname $lastname"            Write-Host "principal=$principal"
+			$Search = "LDAP://CN=Users," + $Root.rootDomainNamingContext
+			$Container =  [ADSI]($Search)
+			$Container
+			$usr = $Container.Create("user","cn=$name")
+			$usr.Put("sAMAccountName",$principal)
+			$usr.CommitChanges()
+
+			$Search = "LDAP://CN=" + $name + ",CN=Users," + $Root.rootDomainNamingContext
+			$Search
+			$objUser = [ADSI]$Search
+			$objUser
+			$password = $user.password
+			Write-Host "password=$password"
+			$objUser.SetPassword($password)
+			$objUser.userAccountControl
+			$objUser.userAccountControl = 66048
+			$objUser.userAccountControl
+			$objUser.CommitChanges()
+		}
 }
