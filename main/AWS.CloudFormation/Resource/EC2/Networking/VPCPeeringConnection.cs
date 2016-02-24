235e87b5815630dc22f0fdb3dce81dc2dbfe29a3
Timothy Bassett <tim@the.bassetts.name>
2016-02-24 07:32:44 -0500

    vpc peering

---
+++ main/AWS.CloudFormation/Resource/EC2/Networking/VPCPeeringConnection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AWS.CloudFormation.Resource.DirectoryService;
+using Newtonsoft.Json;
+
+namespace AWS.CloudFormation.Resource.EC2.Networking
+{
+    public class VpcPeeringConnection : ResourceBase
+    {
+        public VpcPeeringConnection(Vpc vpc, object peerVpcId) : base(ResourceType.AwsEc2VpcPeeringConnection)
+        {
+            this.VpcId = vpc.LogicalId;
+            this.PeerVpcId = peerVpcId;
+        }
+
+        protected override bool SupportsTags => true;
+
+        [JsonIgnore]
+        public string VpcId
+        {
+            get
+            {
+                return this.Properties.GetValue<string>();
+            }
+            private set { this.Properties.SetValue(value); }
+        }
+        [JsonIgnore]
+        public object PeerVpcId
+        {
+            get
+            {
+                return this.Properties.GetValue<object>();
+            }
+            private set { this.Properties.SetValue(value); }
+        }
+    }
+}
