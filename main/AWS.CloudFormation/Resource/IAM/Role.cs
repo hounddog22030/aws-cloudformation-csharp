0595673232d84169d0e7180cd6da61c624cf3da0
Timothy Bassett <tim@the.bassetts.name>
2016-03-05 21:42:39 -0500

    b4 attempting to make role the correct way

---
+++ main/AWS.CloudFormation/Resource/IAM/Role.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AWS.CloudFormation.Common;
+using Newtonsoft.Json;
+
+namespace AWS.CloudFormation.Resource.IAM
+{
+//"Properties": {
+//      "AssumeRolePolicyDocument": { JSON },
+//      "ManagedPolicyArns": [String, ... ],
+//      "Path": String,
+//      "Policies": [Policies, ... ]
+//   }
+    public class Role : ResourceBase
+    {
+        public Role(CloudFormationDictionary rolePolicyDocument, object[] policies, string path) : base(ResourceType.AwsIamRole)
+        {
+            this.AssumeRolePolicyDocument = rolePolicyDocument;
+            this.Path = path;
+            this.Policies = policies;
+        }
+
+        [JsonIgnore]
+        public object[] Policies
+        {
+            get
+            {
+                return this.Properties.GetValue<object[]>();
+            }
+            private set { this.Properties.SetValue(value); }
+        }
+
+        [JsonIgnore]
+        public string Path
+        {
+            get
+            {
+                return this.Properties.GetValue<string>();
+            }
+            private set { this.Properties.SetValue(value); }
+        }
+
+        public Role(CloudFormationDictionary assumeRolePolicyDocument) : base(ResourceType.AwsIamRole)
+        {
+            this.AssumeRolePolicyDocument = assumeRolePolicyDocument;
+        }
+
+        protected override bool SupportsTags => false;
+
+        [JsonIgnore]
+        public object AssumeRolePolicyDocument
+        {
+            get
+            {
+                return this.Properties.GetValue<object>();
+            }
+            private set { this.Properties.SetValue(value); }
+        }
+    }
+}
